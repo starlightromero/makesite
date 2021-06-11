f47f75087d63656b2baa4ca346ab242f9dd87681
Starlight Romero <starlightromero@protonmail.com>
2021-06-11 15:16:07 -0700

    [update] makesite.go
    Render the contents of  using Go Templates and print it to stdout.

--- makesite.go
+++ makesite.go
@@ -7,15 +7,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, world!")
 	fileContents := readFile("first-post.txt")
 	fmt.Print(string(fileContents))
 }
 
 func readFile(file string) []byte {
-	fileContents, err := ioutil.ReadFile(file)
+	content, err := ioutil.ReadFile(file)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return fileContents
+	return content
 }
