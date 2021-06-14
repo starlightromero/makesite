49768c78c500a7b8730437ed0203111e11e0c5f1
Starlight Romero <starlightromero@protonmail.com>
2021-06-14 10:46:31 -0700

    [update] makesite.go
    Write the HTML template to the filesystem to a file. Name it .

--- makesite.go
+++ makesite.go
@@ -1,20 +1,39 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"html/template"
 	"log"
+	"os"
 )
 
+type Data struct {
+	Content string
+}
+
 func main() {
-	fileContents := readFile("first-post.txt")
-	fmt.Print(string(fileContents))
+	fileContent := readFile("first-post.txt")
+	writeTemplate("template.tmpl", "first-post", string(fileContent))
 }
 
 func readFile(file string) []byte {
-	content, err := ioutil.ReadFile(file)
+	content, err := os.ReadFile(file)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return content
+}
+
+func writeTemplate(tmpl, filename, fileContent string) {
+	data := Data{fileContent}
+
+	htmlFile, osErr := os.Create(filename + ".html")
+	if osErr != nil {
+		log.Fatal(osErr)
+	}
+
+	t := template.Must(template.ParseFiles(tmpl))
+	execErr := t.Execute(htmlFile, data)
+	if execErr != nil {
+		log.Fatal(execErr)
+	}
 }
